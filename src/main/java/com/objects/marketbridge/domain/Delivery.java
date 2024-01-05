8122235835236f7d0b1ba64103b6aabd61436af5
Minwoo Jhung <minwoorich@naver.com>
2024-01-05 11:46:11 +0900

    #5 feat : Delivery 연관관계 매핑
    Delivery 연관관계 매핑

--- src/main/java/com/objects/marketbridge/domain/Delivery.java
+++ src/main/java/com/objects/marketbridge/domain/Delivery.java
@@ -21,12 +21,17 @@ public class Delivery extends BaseEntity {
     @Enumerated(EnumType.STRING)
     private DeliveryType deliveryType; // NORMAL, EXCHANGE, RETURN // column이름이랑 다름
 
-    // TODO
-    private Long sellerId;
-    // TODO
-    private Long addressId;
-    // TODO
-    private Long orderDetailId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "seller_id")
+    private Seller sellerId;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "address_id")
+    private Address addressId;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "address_id")
+    private ProdOrderDetail orderDetailId;
 
     private String carrier;
 
@@ -39,7 +44,7 @@ public class Delivery extends BaseEntity {
     private LocalDateTime deliveredDate;
 
     @Builder
-    private Delivery(DeliveryType deliveryType, Long sellerId, Long addressId, Long orderDetailId, String carrier, String trackingNo, String status, LocalDateTime shipDate, LocalDateTime deliveredDate) {
+    private Delivery(DeliveryType deliveryType, Seller sellerId, Address addressId, ProdOrderDetail orderDetailId, String carrier, String trackingNo, String status, LocalDateTime shipDate, LocalDateTime deliveredDate) {
         this.deliveryType = deliveryType;
         this.sellerId = sellerId;
         this.addressId = addressId;
